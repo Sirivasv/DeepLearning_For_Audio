7deacd78f405dda458dbb08ac6021df4a595fb01
Saul Ivan Rivas Vega <saul.ivan.rivas.vega@gmail.com>
2020-07-16 18:41:40 -0500

    added uwsgi.ini

--- SPEECHWEBAPP/client.py
+++ SPEECHWEBAPP/client.py
@@ -1,8 +1,8 @@
 import requests
 
-URL = "http://127.0.0.1:5000/predict"
-TEST_AUDIO_FILE_PATH = "./dataset/left/1d919a90_nohash_1.wav"
-#TEST_AUDIO_FILE_PATH = "./dataset/on/1aed7c6d_nohash_1.wav"
+URL = "http://54.146.79.152:5000/predict"
+#TEST_AUDIO_FILE_PATH = "./dataset/left/1d919a90_nohash_1.wav"
+TEST_AUDIO_FILE_PATH = "./dataset/on/1aed7c6d_nohash_1.wav"
 
 if __name__ == "__main__":
     
