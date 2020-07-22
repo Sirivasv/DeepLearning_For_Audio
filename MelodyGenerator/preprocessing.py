1cb598dff5fcdcda67d57a494a1a484a9b0e68a1
Saul Ivan Rivas Vega <saul.ivan.rivas.vega@gmail.com>
2020-07-22 08:52:58 -0500

    added debug settings and las preprocessing task

--- MelodyGenerator/preprocessing.py
+++ MelodyGenerator/preprocessing.py
@@ -1,7 +1,14 @@
 import os
+import json
 import music21 as m21
+import numpy as np
+import tensorflow.keras as keras
 
 KERN_DATASET_PATH = "./data/deutschl/test/"
+SAVE_PREPROCESSING_DIR = "./data/dataset/"
+SINGLE_FILE_DATASET = "./data/file_dataset"
+SEQUENCE_LENGTH = 64
+MAPPING_PATH = "./data/mapping.json"
 ACCEPTABLE_DURATIONS = [
     0.25,
     0.5,
@@ -13,14 +20,12 @@ ACCEPTABLE_DURATIONS = [
     4
 ]
 
-SAVE_PREPROCESSING_DIR = "./data/dataset/"
-
 def load_songs_in_kern(dataset_path):
 
     # Go through all the files in the dataset and load them with music21
     songs = []
 
-    for path, subdirs, files in os.walk(dataset_path):
+    for path, _, files in os.walk(dataset_path):
         for file_i in files:
             if file_i[-3:] == "krn":
                 song = m21.converter.parse(os.path.join(path, file_i))
@@ -113,15 +118,103 @@ def preprocess(dataset_path):
         with open(save_path, "w") as fp:
             fp.write(encoded_song)
 
+def load(file_path):
+
+    with open(file_path, "r") as fp:
+        song = fp.read()
+
+    return song
+
+def create_single_file_dataset(dataset_path, file_dataset_path, sequence_length):
+
+    new_song_delimiter = "/ " * sequence_length
+    songs = ""
+
+    # Load encoded songs and add delimiters
+    for path, _, files in os.walk(dataset_path):
+        for file in files:
+            file_path = os.path.join(path, file)
+            song = load(file_path)
+            songs = songs + song + " " + new_song_delimiter
+    songs = songs[:-1]
+
+    # Save string that contains all dataset
+    with open(file_dataset_path, "w") as fp:
+        fp.write(songs)
+
+    return songs
+
+def create_mapping(songs, mapping_path):
+
+    mappings = {}
+
+    # Identify the vocabulary
+    songs = songs.split()
+    vocabulary = list(set(songs))
+
+    # Create mappings
+    for i, symbol in enumerate(vocabulary):
+        mappings[symbol] = i
+
+    # Save vocabulary to a json file
+    with open(mapping_path, "w") as fp:
+        json.dump(mappings, fp, indent=4)
+
+def convert_songs_to_int(songs):
+
+    int_songs = []
+
+    # Load the mappings
+    with open(MAPPING_PATH, "r") as fp:
+        mappings = json.load(fp)
+
+    # Cast songs string to a list
+    songs = songs.split()
+
+    # Map songs to int
+    for symbol in songs:
+        int_songs.append(mappings[symbol])
+    
+    return int_songs
+
+def generate_training_sequences(sequence_length):
+
+    # [11, 12, 13, 14, ...] -> i: [11, 12], t: 13; i: [12, 13] t: 14
+
+    # Load songs and map to int
+    songs = load(SINGLE_FILE_DATASET)
+    int_songs = convert_songs_to_int(songs)
+
+    # Generate the training sequences
+    # 100 symbols, 64 sl, 100 - 64 = 36
+    inputs = []
+    targets = []
+
+    num_sequences = len(int_songs) - sequence_length
+    for i in range(num_sequences):
+        inputs.append(int_songs[i:i+sequence_length])
+        targets.append(int_songs[i+sequence_length])
+
+    # One-hot encode the sequences
+    # inputs.shape = (# of sequences, sequence_length, vocabulary size)
+    # [ [0, 1, 2], [1, 1, 2] ] -> [ [ [1, 0, 0], [0, 1, 0], [0,0,1] ], ... [] ]
+    vocabulary_size = len(set(int_songs))
+    inputs = keras.utils.to_categorical(inputs, num_classes=vocabulary_size)
+    targets = np.array(targets)
+
+    return inputs, targets
+
+def main():
+    preprocess(KERN_DATASET_PATH)
+    songs = create_single_file_dataset(
+        SAVE_PREPROCESSING_DIR,
+        SINGLE_FILE_DATASET,
+        SEQUENCE_LENGTH
+    )
+    create_mapping(songs, MAPPING_PATH)
+
+    inputs, targets = generate_training_sequences(SEQUENCE_LENGTH)
 
 if __name__ == "__main__":
 
-    songs = load_songs_in_kern(KERN_DATASET_PATH)
-    print(F"Loaded {len(songs)} songs.")
-    song = songs[0]
-    
-    preprocess(KERN_DATASET_PATH)
-
-    # Transpose Song
-    transposed_song = transpose(song)
-    transposed_song.show()
+    main()
